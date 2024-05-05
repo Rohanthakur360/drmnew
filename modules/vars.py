61aed09862a90d3080a39f92aea487aaf5dd5ee4
StubbornDEvil <70991755+Rohanthakur360@users.noreply.github.com>
2024-05-05 14:56:53 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
 api_id = 20963688
 api_hash = "964a26dc404aa5e12dfe82ea94ae7ae3"
-bot_token = "7061439554:AAHDXBBktV5P4O6sjBLamdX2hhvBN7uPSl0"
+bot_token = "6499994285:AAFaSySS9Fc50k9-CKvOPlZ5-Fcv7ZH7b28"
 # Bot Username = "allinone"
